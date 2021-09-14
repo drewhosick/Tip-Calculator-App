e4c1b7b4cd6f7d0bdb8fa124317ea4c06d90151e
drewhosick <drewhosick@gmail.com>
2021-09-14 17:52:46 -0400

    Updated More code

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
 const tipSize = document.querySelectorAll("li");
 const customTip = document.querySelector(".customLi");
-const customTipInput = document.querySelector(".liInput")
+const billAmount = document.querySelector(".inputBillAmount");
+const numberOfPeople = document.querySelector(".inputNumberOfPeople");
+const tipAmountPerPerson = document.querySelector(".tipPerPersonAmountText");
+const totalAmountPerPerson = document.querySelector(".totalPerPersonAmountText");
+const resetButton = document.querySelector(".resetBox");
+
+
 
 
 tipSize.forEach(tip => {
     tip.addEventListener('click', e => {
         for (let i = 0; i < tipSize.length; i++) {
-            customTipInput.classList.remove("addSelectorColor");
             tipSize[i].classList.remove("addSelectorColor");
         }
-        tipSize
         e.target.classList.add("addSelectorColor");
         
 
     });
 });
 
+resetButton.addEventListener('click', () => {
+    billAmount.value = "142.55";
+    numberOfPeople.value = "5";
+    for (let i = 0; i < tipSize.length; i++) {
+        // customTip.classList.remove("addSelectorColor");
+        tipSize[i].classList.remove("addSelectorColor");
+        tipSize[2].classList.add("addSelectorColor"); // reset to 15% highlighted
+    }
+});
